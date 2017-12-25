aea59af54f504e08b933e6355ea607f27de50c33
nwmei <nwmei@bu.edu>
2017-12-24 23:48:46 -0500

    setting up platformer game, Game class and its methods defined but incomplete.

---
+++ Exploria Platformer Game/main.py
@@ -0,0 +1,37 @@
+#Exploria Platform Game
+import pygame
+import random
+import os
+from settings import *
+
+class Game:
+    def __init__(self):
+        """initialize game window, etc"""
+        pass
+    
+    def new(self):
+        """start a new game"""
+        pass
+    
+    def run(self):
+        """game loop"""
+        pass
+
+    def update(self):
+        """game loop update"""
+        pass
+    
+    def events(self):
+        """game loop events"""
+        pass
+    
+    def draw(self):
+        """game loop draw"""
+        pass
+
+g = Game()
+g.show_start_screen()
+while True:
+    g.new()
+
+    
---
+++ Exploria Platformer Game/settings.py
@@ -0,0 +1,12 @@
+#game options and settings
+TITLE = 'Exploria'
+WIDTH = 480
+HEIGHT = 600
+FPS = 60
+
+#define common colors
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+RED = (255,0,0)
+GREEN = (0,255,0)
+BLUE = (0,0,255)
