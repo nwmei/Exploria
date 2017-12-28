16592d351552faa10179d95c1200488d8c2e4d50
nwmei <nwmei@bu.edu>
2017-12-28 00:33:00 -0500

    added Platform sprite class, and updated Player class to handle falling

--- Exploria Platformer Game/sprites.py
+++ Exploria Platformer Game/sprites.py
@@ -14,14 +14,34 @@ class Player(pg.sprite.Sprite):
         self.acc = vec(0, 0)
 
     def update(self):
-        self.acc = vec(0, 0)
+        self.acc = vec(0, 0.5)
         keys = pg.key.get_pressed()
         if keys[pg.K_LEFT]:
-            self.acc.x = -0.5
+            self.acc.x = -PLAYER_ACC
         if keys[pg.K_RIGHT]:
-            self.acc.x = 0.5
+            self.acc.x = PLAYER_ACC
 
+        #apply friction
+        self.acc.x += self.vel.x * PLAYER_FRICTION
+
+        #equations of motion
         self.vel += self.acc
         self.pos += self.vel + 0.5*self.acc
 
-        self.rect.center = self.pos
+        #wrap screen
+        if self.pos.x > WIDTH:
+            self.pos.x = 0
+        if self.pos.x < 0:
+            self.pos.x = WIDTH
+
+        self.rect.midbottom = self.pos
+
+
+class Platform(pg.sprite.Sprite):
+    def __init__(self, x, y, w, h):
+        pg.sprite.Sprite.__init__(self)
+        self.image = pg.Surface((w, h))
+        self.image.fill(GREEN)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
