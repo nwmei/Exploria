52c3cbd2c9edbd66ce0a854e8292cc374f2c6695
nwmei <nwmei@bu.edu>
2017-12-30 23:21:37 -0500

    player jump method

--- Exploria Platformer Game/sprites.py
+++ Exploria Platformer Game/sprites.py
@@ -3,9 +3,10 @@ from settings import *
 vec = pg.math.Vector2
 
 class Player(pg.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, game):
         pg.sprite.Sprite.__init__(self)
-        self.image = pg.Surface((30,40))
+        self.game = game
+        self.image = pg.Surface((30, 40))
         self.image.fill(YELLOW)
         self.rect = self.image.get_rect()
         self.rect.center = (WIDTH/2, HEIGHT/2)
@@ -13,22 +14,26 @@ class Player(pg.sprite.Sprite):
         self.vel = vec(0, 0)
         self.acc = vec(0, 0)
 
+    def jump(self):
+        self.vel.y = -14
+
+
     def update(self):
-        self.acc = vec(0, 0.5)
+        self.acc = vec(0, PLAYER_GRAV)
         keys = pg.key.get_pressed()
         if keys[pg.K_LEFT]:
             self.acc.x = -PLAYER_ACC
         if keys[pg.K_RIGHT]:
             self.acc.x = PLAYER_ACC
 
-        #apply friction
+        # apply friction
         self.acc.x += self.vel.x * PLAYER_FRICTION
 
-        #equations of motion
+        # equations of motion
         self.vel += self.acc
         self.pos += self.vel + 0.5*self.acc
 
-        #wrap screen
+        # wrap screen
         if self.pos.x > WIDTH:
             self.pos.x = 0
         if self.pos.x < 0:
