167a24c43f9ab92725ce392143ea7c35eb2b9553
nwmei <nwmei@bu.edu>
2017-12-31 23:50:56 -0500

    enhanced method to check for collisions by focusing on exact collision points

--- Exploria Platformer Game/main.py
+++ Exploria Platformer Game/main.py
@@ -11,12 +11,12 @@ class Game:
         """initialize game window, etc"""
         pg.init()
         pg.mixer.init()
-        self.screen = pg.display.set_mode((WIDTH,HEIGHT))
+        self.screen = pg.display.set_mode((WIDTH, HEIGHT))
         pg.display.set_caption(TITLE)
         self.clock = pg.time.Clock()
         self.running = True
-
-        self.last_update = pg.time.get_ticks()
+        self.last_vertical_correction = pg.time.get_ticks()
+        self.platform_rect_list = []
 
     def new(self):
         """start a new game"""
@@ -25,15 +25,17 @@ class Game:
         self.player = Player(self)
         self.all_sprites.add(self.player)
 
-        #create platforms, starting with base
+        # create platforms, starting with base
         self.base_platform = Platform(-3000, HEIGHT-40, 16350, 300) #width was W*10
         self.all_sprites.add(self.base_platform)
         self.platforms.add(self.base_platform)
-        #other platforms
+        self.platform_rect_list.append(self.base_platform.rect)
+        # other platforms
         for platform in PLATFORM_LIST:
             plat = Platform(*platform)
             self.all_sprites.add(plat)
             self.platforms.add(plat)
+            self.platform_rect_list.append(plat.rect)
         self.run()
     
     def run(self):
@@ -48,20 +50,45 @@ class Game:
     def update(self):
         """game loop update"""
         self.all_sprites.update()
+        # # if player hits surface of platform
+        # if self.player.vel.y > 0:
+        #     hits = pg.sprite.spritecollide(self.player, self.platforms, False)
+        #     if hits:
+        #         self.player.pos.y = hits[0].rect.top+1
+        #         self.player.vel.y = 0
+        # # if player hits bottom of platform (from jumping)
+        # if self.player.vel.y < 0:
+        #     hits = pg.sprite.spritecollide(self.player, self.platforms, False)
+        #     if hits:
+        #         self.player.pos.y = hits[0].rect.bottom+40
+        #         self.player.vel.y = 0
 
-        # if player hits surface of platform
-        if self.player.vel.y > 0:
-            hits = pg.sprite.spritecollide(self.player, self.platforms, False)
-            if hits:
-                self.player.pos.y = hits[0].rect.top+1
-                self.player.vel.y = 0
-        # if player hits bottom of platform (from jumping)
-        if self.player.vel.y < 0:
-            hits = pg.sprite.spritecollide(self.player, self.platforms, False)
-            if hits:
-                self.player.pos.y = hits[0].rect.bottom+40
-                self.player.vel.y = 0
-                self.player.acc.y = 0
+        # if player hits side of platforms
+        platform_collision_index = self.player.rect.collidelist(self.platform_rect_list)
+
+        collision_point_list = self.player.check_collision(self.platform_rect_list[platform_collision_index])
+
+        # left side
+        if collision_point_list[5] == 1 and self.player.vel.x > 0:
+            self.player.pos.x = self.platform_rect_list[platform_collision_index].left - 1
+            self.player.vel.x = 0
+
+        # right side
+        if collision_point_list[4] == 1 and self.player.vel.x < 0:
+            self.player.pos.x = self.platform_rect_list[platform_collision_index].right + 1
+            self.player.vel.x = 0
+
+        # on top of platform
+        if (collision_point_list[2] == 1 or collision_point_list[3] == 1 or collision_point_list[7] == 1) \
+                and self.player.vel.y > 0:
+            self.player.pos.y = self.platform_rect_list[platform_collision_index].top + 1
+            self.player.vel.y = 0
+
+        # under platform
+        if (collision_point_list[0] == 1 or collision_point_list[1] == 1 or collision_point_list[6] == 1) \
+                and self.player.vel.y < 0:
+            self.player.pos.y = self.platform_rect_list[platform_collision_index].bottom - 1
+            self.player.vel.y = 0
 
         # if player reaches top 1/4 of screen
         if self.player.rect.top <= HEIGHT/6 or self.player.rect.top >= HEIGHT/2:
@@ -69,12 +96,12 @@ class Game:
             for platform in self.platforms:
                 platform.rect.y -= self.player.vel.y
 
-        #correction of y coordinates of player
+        # correction of y coordinates of player
         now = pg.time.get_ticks()
-        if now - self.last_update > 10:
-            self.last_update = now
+        if now - self.last_vertical_correction > 10:
+            self.last_vertical_correction = now
             if abs(self.player.pos.y - HEIGHT/2) > 2:
-                if HEIGHT/2 - self.player.pos.y < 0: #player below middle
+                if HEIGHT/2 - self.player.pos.y < 0:
                     sign = -1
                 else:
                     sign = 1
@@ -82,7 +109,7 @@ class Game:
                 for platform in self.platforms:
                     platform.rect.y += sign
 
-
+        # scroll map horizontally while player moves
         self.player.pos.x -= self.player.vel.x
         for platform in self.platforms:
             platform.rect.x -= int(self.player.vel.x)
