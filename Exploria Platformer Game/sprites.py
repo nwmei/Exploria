dbb89365bb5d7d630aaf1c0f54289077f0b4bb14
nwmei <nwmei@bu.edu>
2018-01-01 21:38:06 -0500

    added player to platform collision behavior such as falling on a platform

--- Exploria Platformer Game/sprites.py
+++ Exploria Platformer Game/sprites.py
@@ -1,6 +1,8 @@
 import pygame as pg
 from settings import *
+
 vec = pg.math.Vector2
+
 
 class Player(pg.sprite.Sprite):
     def __init__(self, game):
@@ -9,8 +11,8 @@ class Player(pg.sprite.Sprite):
         self.image = pg.Surface((30, 40))
         self.image.fill(YELLOW)
         self.rect = self.image.get_rect()
-        self.rect.center = (WIDTH/2, HEIGHT/2)
-        self.pos = vec(WIDTH/2, HEIGHT/2)
+        self.rect.center = (WIDTH / 2, HEIGHT / 2)
+        self.pos = vec(WIDTH / 2, HEIGHT / 2)
         self.vel = vec(0, 0)
         self.acc = vec(0, 0)
         self.collision = [False] * 9
@@ -29,8 +31,32 @@ class Player(pg.sprite.Sprite):
         self.acc.x += self.vel.x * PLAYER_FRICTION
         # equations of motion
         self.vel += self.acc
-        self.pos += self.vel + 0.5*self.acc
+        self.pos += self.vel + 0.5 * self.acc
         self.rect.midbottom = self.pos
+
+        # find which platform rect the player is colliding with
+        platform_collision_index = self.rect.collidelist(self.game.platform_rect_list)
+        collision_point_list = self.check_collision(self.game.platform_rect_list[platform_collision_index])
+
+        # on top of platform
+        if (collision_point_list[2] == 1 or collision_point_list[3] == 1 or collision_point_list[7] == 1) \
+                and collision_point_list[5] != 1 and collision_point_list[4] != 1 and self.vel.y > 0:
+            self.pos.y = self.game.platform_rect_list[platform_collision_index].top + 1
+            self.vel.y = 0
+        # under platform
+        if (collision_point_list[0] == 1 or collision_point_list[1] == 1 or collision_point_list[6] == 1) \
+                and collision_point_list[5] != 1 and collision_point_list[4] != 1 and self.vel.y < 0:
+            self.pos.y = self.game.platform_rect_list[platform_collision_index].bottom + 40
+            self.vel.y = 0
+        # left side
+        if collision_point_list[5] == 1 and self.vel.x > 0:
+            self.pos.x = self.game.platform_rect_list[platform_collision_index].left - 1
+            self.vel.x = 0
+        # right side
+        if collision_point_list[4] == 1 and self.vel.x < 0:
+            self.pos.x = self.game.platform_rect_list[platform_collision_index].right + 1
+            self.vel.x = 0
+
 
     def check_collision(self, platform_rect):
         self.collision[0] = platform_rect.collidepoint(self.rect.topleft)
@@ -42,6 +68,7 @@ class Player(pg.sprite.Sprite):
         self.collision[6] = platform_rect.collidepoint(self.rect.midtop)
         self.collision[7] = platform_rect.collidepoint(self.rect.midbottom)
         return self.collision
+
 
 class Platform(pg.sprite.Sprite):
     def __init__(self, x, y, w, h):
